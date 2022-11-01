e50f7fb3e97b7fda43c09138f92b01d0046ee90c
Salim66 <salimhasanriad8@gmail.com>
2022-11-01 22:17:31 +0600

    Recent Project Section Part 2

--- user/src/components/RecentProjects/RecentProjects.jsx
+++ user/src/components/RecentProjects/RecentProjects.jsx
@@ -7,34 +7,34 @@ class RecentProjects extends Component {
     return (
       <>
         <Container>
-            <h1 className="serviceMainTitle text-center">MY SERVICES</h1>
+            <h1 className="serviceMainTitle text-center">RECENT PROJECTS</h1>
             <Row>
                 <Col sm={12} md={6} lg={4}>
-                    <Card>
+                    <Card className='projectCard'>
                         <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=839,format=auto/uploads/project/373921/cover_1242_a7a72bdcab531d2f78891196ab4f77e8.jpg" alt="" />
                         <Card.Body>
-                            <Card.Title>New Project</Card.Title>
-                            <Card.Text> Lorem ipsum dolor sit amet. </Card.Text>
+                            <Card.Title className='projectCardTitle'>New Project</Card.Title>
+                            <Card.Text className='projectCardDes'> Lorem ipsum dolor sit amet. </Card.Text>
                             <Button>Read More</Button>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col sm={12} md={6} lg={4}>
-                    <Card>
+                    <Card className='projectCard'>
                         <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=839,format=auto/uploads/project/373921/cover_1242_a7a72bdcab531d2f78891196ab4f77e8.jpg" alt="" />
                         <Card.Body>
-                            <Card.Title>New Project</Card.Title>
-                            <Card.Text> Lorem ipsum dolor sit amet. </Card.Text>
+                            <Card.Title className='projectCardTitle'>New Project</Card.Title>
+                            <Card.Text className='projectCardDes'> Lorem ipsum dolor sit amet. </Card.Text>
                             <Button>Read More</Button>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col sm={12} md={6} lg={4}>
-                    <Card>
+                    <Card className='projectCard'>
                         <img src="https://static.skillshare.com/cdn-cgi/image/quality=85,width=1242,height=839,format=auto/uploads/project/373921/cover_1242_a7a72bdcab531d2f78891196ab4f77e8.jpg" alt="" />
                         <Card.Body>
-                            <Card.Title>New Project</Card.Title>
-                            <Card.Text> Lorem ipsum dolor sit amet. </Card.Text>
+                            <Card.Title className='projectCardTitle'>New Project</Card.Title>
+                            <Card.Text className='projectCardDes'> Lorem ipsum dolor sit amet. </Card.Text>
                             <Button>Read More</Button>
                         </Card.Body>
                     </Card>
