2015e6479c23ec015c0bda54eee818e3b20321f4
Salim66 <salimhasanriad8@gmail.com>
2022-11-01 14:20:09 +0600

    Paralex Summary Section part 2

--- user/src/components/Summary/Summary.jsx
+++ user/src/components/Summary/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import './Summary.css';
 
 class Summary extends Component {
@@ -8,7 +8,27 @@ class Summary extends Component {
       <>
         <Container fluid={true} className="summaryBanner summarySection p-0" >
             <div className="summaryBannerOverlay">
-                
+                <Container>
+                    <Row>
+                        <Col sm={12} md={6} lg={8}>
+                            <Row className='text-center'>
+                                <Col>
+                                    <h1>100</h1>
+                                    <h4>Total Projects</h4>
+                                    <hr className="bg-white w-25" />
+                                </Col>
+                                <Col>
+                                    <h1>100</h1>
+                                    <h4>Total Projects</h4>
+                                    <hr className="bg-white w-25" />
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col sm={12} md={6} lg={4}>
+
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </Container>
       </>
