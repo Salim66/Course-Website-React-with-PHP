20792dfb89d95786bb676fb5a6ce9455435725af
Salim66 <salimhasanriad8@gmail.com>
2022-11-02 21:15:09 +0600

    Client Review Section Part 1

---
+++ user/src/components/ClientReview/ClientReview.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './ClientReview.css';
+
+class ClientReview extends Component {
+  render() {
+    return (
+      <>
+
+      </>
+    )
+  }
+}
+
+export default ClientReview;
