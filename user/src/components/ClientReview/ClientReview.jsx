ff026d78d209962ecd2316e8c24d3d183a1bc73b
Salim66 <salimhasanriad8@gmail.com>
2022-11-02 21:23:01 +0600

    Client Review Section Part 2

--- user/src/components/ClientReview/ClientReview.jsx
+++ user/src/components/ClientReview/ClientReview.jsx
@@ -5,6 +5,43 @@ import './ClientReview.css';
 
 class ClientReview extends Component {
   render() {
+    var settings = {
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    };
     return (
       <>
 
