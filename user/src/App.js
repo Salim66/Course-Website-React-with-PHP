b2dbb7712a55f99a36f381909b9e7dc009223f07
Salim66 <salimhasanriad8@gmail.com>
2022-11-05 22:29:39 +0600

    Router Configuration done

--- user/src/App.js
+++ user/src/App.js
@@ -7,17 +7,20 @@ import AllProjectsPage from "./pages/AllProjectsPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import ServicePage from "./pages/ServicePage";
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      {/* <AboutPage/> */}
-      {/* <AllCoursesPage/> */}
-      {/* <AllProjectsPage /> */}
-      {/* <ContactPage /> */}
-      <ServicePage />
+      <Routes>
+        <Route exact path="/" element={ <HomePage /> } />
+        <Route exact path="/services" element={ <ServicePage /> } />
+        <Route exact path="/courses" element={ <AllCoursesPage/> } />
+        <Route exact path="/portfolio" element={ <AllProjectsPage /> } />
+        <Route exact path="/courses" element={ <ContactPage /> } />
+        <Route exact path="/about" element={ <AboutPage/> } />
+      </Routes>      
     </>
   );
 }
